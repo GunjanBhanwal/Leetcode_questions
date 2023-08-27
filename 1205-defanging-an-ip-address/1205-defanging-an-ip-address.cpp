d3b2c6c9d85f46eeb0036781de82b35d0eb77000
Gunjan Bhanwal <133032453+GunjanBhanwal@users.noreply.github.com>
2023-08-27 20:16:10 +0530

    Time: 2 ms (45.22%), Space: 6 MB (23.08%) - LeetHub

--- 1205-defanging-an-ip-address/1205-defanging-an-ip-address.cpp
+++ 1205-defanging-an-ip-address/1205-defanging-an-ip-address.cpp
@@ -4,10 +4,10 @@ public:
         string ans;
         for(int i=0;i<address.size();i++){
             if(address[i]=='.'){
-               ans=ans+"[.]";
+               ans+="[.]";
             }
             else{
-                ans=ans+address[i];
+                ans+=address[i];
             }
         }
         return ans;
