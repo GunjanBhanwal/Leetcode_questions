377712943bc95020ed13a5971b47cd88340e2914
Gunjan Bhanwal <133032453+GunjanBhanwal@users.noreply.github.com>
2023-09-06 00:05:08 +0530

    Time: 10 ms (14.95%), Space: 12.5 MB (44.23%) - LeetHub

--- 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.cpp
+++ 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.cpp
@@ -5,7 +5,11 @@ public:
         int a,b,ans;
         a=nums[0];
         b=nums[nums.size()-1];
-        int c;
-        return gcd(a,b);
+        for(int i=1;i<=a;i++){
+            if(a%i==0 && b%i==0){
+                ans=i;
+            }
+        }
+        return ans;
     }
 };
